a40ed47ebfc296bf087b01b64863ac09cd0751fd
chojmi <chojnackimichal@hotmail.com>
2016-02-02 21:29:00 +0100

    Added unit tests.

--- app/src/androidTest/java/michalchojnacki/magazynbmp/controllers/dbControllers/SparePartsDbControllerTest.java
+++ app/src/androidTest/java/michalchojnacki/magazynbmp/controllers/dbControllers/SparePartsDbControllerTest.java
@@ -24,28 +24,27 @@ public class SparePartsDbControllerTest {
     @Before
     public void initSparePartsDbController() {
         sparePartsDbController = new SparePartsDbController(InstrumentationRegistry.getTargetContext());
-
+        SparePart sparePart = new SparePart.Builder().number("YA2020").build();
+        sparePartsDbController.saveSparePart(sparePart);
     }
 
     @Test
-    public void countOfEmptyDb() {
+    public void countOfSparePartsAfterDeletingAll() {
+        sparePartsDbController.deleteAllSpareParts();
         assertThat(sparePartsDbController.getCountOfSpareParts(), equalTo(0));
     }
 
     @Test
-    public void testSaveSparePart() {
-        SparePart sparePart = new SparePart.Builder().number("YA2020").build();
+    public void countOfSparePartsAfterSaving() {
+        SparePart sparePart = new SparePart.Builder().number("YA2010").build();
 
         sparePartsDbController.saveSparePart(sparePart);
 
-        assertThat(sparePartsDbController.getCountOfSpareParts(), equalTo(1));
+        assertThat(sparePartsDbController.getCountOfSpareParts(), equalTo(2));
     }
 
     @Test
     public void testFindSparePartByString() {
-        SparePart sparePart = new SparePart.Builder().number("YA2020").build();
-        sparePartsDbController.saveSparePart(sparePart);
-
         SparePart[] spareParts = sparePartsDbController.findSparePart("YA2020");
 
         assertFalse(spareParts.length != 1);
@@ -53,14 +52,29 @@ public class SparePartsDbControllerTest {
     }
 
     @Test
-    public void testFindSparePartBySparePart() {
+    public void findSparePartBySparePartWhenIsPresent() {
         SparePart sparePart = new SparePart.Builder().number("YA2020").build();
-        sparePartsDbController.saveSparePart(sparePart);
 
         SparePart[] spareParts = sparePartsDbController.findSparePart(sparePart);
 
         assertFalse(spareParts.length != 1);
         assertThat(spareParts[0].getNumber(), equalTo("YA2020"));
+    }
+
+    @Test
+    public void findSparePartBySparePartWhenIsNotPresent() {
+        SparePart sparePart = new SparePart.Builder().number("YA2021").build();
+
+        SparePart[] spareParts = sparePartsDbController.findSparePart(sparePart);
+
+        assertThat(spareParts, equalTo(null));
+    }
+
+    @Test
+    public void findSparePartWhenNull() {
+        SparePart[] spareParts = sparePartsDbController.findSparePart((String) null);
+
+        assertThat(spareParts, equalTo(null));
     }
 
     @After
